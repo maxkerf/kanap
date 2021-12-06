999df5a9a13f7927f2876f056094686c681a14be
Maxime Kerfourn <maxime.kerfourn@orange.fr>
2021-12-06 10:33:38 +0100

    update CartProduct class description

--- front/js/models/CartProduct.js
+++ front/js/models/CartProduct.js
@@ -2,6 +2,7 @@ class CartProduct {
 	/**
 	 * Create a cart product with the given data.
 	 * @param {Object} data
+	 * @param {number} data.id - The id of the cart product.
 	 * @param {string} data.productId - The id of the product.
 	 * @param {string} data.color
 	 * @param {number} data.quantity
